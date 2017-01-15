461b66ea86b5eb30f43eeaf49c713904d7e4ecd0
Paul Haley <paul.haley@uqconnect.edu.au>
2017-01-15 19:10:28 +0800

    GUI is now rendering with no functionality (intended at this stage). GUI is not in its final form, minimum viable product. Reset and help buttons are implemented.

--- src/gui/LearningController.java
+++ src/gui/LearningController.java
@@ -7,9 +7,41 @@ import java.io.StringWriter;
 import javax.swing.JComboBox;
 
 public class LearningController {
+	
+	private LearningView view;
+	private LearningModel model;
 
 	public LearningController(LearningModel model, LearningView view) {
-		// TODO Auto-generated constructor stub
+		// Bind instance variables to passed view and model.
+		this.view = view;
+		this.model = model;
+		
+		view.helpListener(new HelpButtonActionListener());
+		view.resetListener(new ResetButtonActionListener());
 	}
 
+	/**
+     * This class listens to the button used to bring up help.
+     */
+    private class HelpButtonActionListener implements ActionListener {
+    	/**
+    	 * Brings up the help dialog box when the help button is pressed.
+    	 */
+    	public void actionPerformed(ActionEvent e) {
+    		view.helpUsage();
+    	}
+    }
+    
+    /**
+     * This class listens to the reset button to reset all training options to 
+     * default.
+     */
+    private class ResetButtonActionListener implements ActionListener {
+    	/**
+    	 * Resets all training options to their defaults.
+    	 */
+    	public void actionPerformed(ActionEvent e) {
+    		view.resetOptions();
+    	}
+    }
 }
--- src/gui/LearningView.java
+++ src/gui/LearningView.java
@@ -17,6 +17,15 @@ public class LearningView extends JFrame {
 	/*
 	 * Creates selection box used to select options applied.
 	 */
+	private String[] svmTypes = {"0 -- C-SVC (multi-class classification)", 
+			"1 -- nu-SVC (multi-class classification)", "2 -- one-class SVM", 
+			"3 -- epsilon-SVR (regression)", "4 -- nu-SVR (regression)"};
+	private String[] kernelTypes = {"0 -- linear: u'*v\n", 
+			"1 -- polynomial: (gamma*u'*v + coef0)^degree\n",
+			"2 -- radial basis function: exp(-gamma*|u-v|^2)\n",
+			"3 -- sigmoid: tanh(gamma*u'*v + coef0)\n",
+			"4 -- precomputed kernel (kernel values in training_set_file)\n"};
+	private String[] yesNo = {"No", "Yes"};
     private JComboBox<String> svmType, kernelType, shrinking, 
     		probabilityEstimates;
     private JTextField degree, gamma, coef0, cost, nu, epsilonLoss, cacheSize, 
@@ -25,8 +34,9 @@ public class LearningView extends JFrame {
     /*
      * Inputs and Outputs
      */
-    private JTextField dataFilepath, modelFilepath, dialog; 
-    private JButton train, help;
+    private JTextField dataFilepath, modelFilepath;
+    private JTextArea dialog;
+    private JButton scale, train, predict, help, reset;
     
     /**
      * Creates a new svm_gui window.
@@ -60,21 +70,239 @@ public class LearningView extends JFrame {
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
         
         //Adds all interface components to the JPanel.
+		addFileSelector(panel);
+		addCombinationSettingHelp(panel);
+		addModes(panel);
 		
+		// Implement and add dialog (output box)
+		dialog = new JTextArea(15,50);
+		panel.add(dialog);
         
         //Adds the highest level JPanel to the container.
         container.add(panel,"North");
 	}
 	
 	/**
-	 * Adds 
+	 * Adds the data filepath selector.
 	 * 
 	 * @param parent JPanel to contain the JPanel constructed.
 	 */
-	private void add(JPanel parent) {
-		
-		return;
+	private void addFileSelector(JPanel parent) {
+		//Creates a new JPanel for labels with a 2 by 2 grid.
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2,2));
+        
+        // Implementing data and model TextFields
+        dataFilepath = new JTextField(50);
+        modelFilepath = new JTextField(50);
+        
+        panel.add(new JLabel("Data file to use:"));
+        panel.add(dataFilepath);
+        
+        panel.add(new JLabel("Model file to use:"));
+        panel.add(modelFilepath);
+        
+        //TODO: Use an actual file selector.
+        
+        parent.add(panel);
 	}
+	
+	/**
+	 * Adds combination of the SVM setting panel and the panel with the 
+	 * restrictions and help button.
+	 * 
+	 * @param parent JPanel to contain the JPanel constructed.
+	 */
+	private void addCombinationSettingHelp(JPanel parent) {
+		JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2,1));
+		
+        addOptions(panel);
+        addRestrictionsHelp(panel);
+        
+        parent.add(panel);
+	}
+	
+	/**
+	 * Adds all inputs for the SVM creation.
+	 * 
+	 * @param parent JPanel to contain the JPanel constructed.
+	 */
+	private void addOptions(JPanel parent) {
+		/*
+		 * Creates a new JPanel for labels with a 4 by 8 grid, effectively only 
+		 * 4 by 4 as every option will have a textfield or a dropbox next to it.
+		 */
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4,8));
+        
+        // Setting up drop downboxes
+        svmType = new JComboBox<String>(svmTypes);
+        kernelType = new JComboBox<String>(kernelTypes);
+        shrinking = new JComboBox<String>(yesNo);
+        probabilityEstimates = new JComboBox<String>(yesNo);
+        
+        //TODO: Put tooltips for each textfield.
+        //TODO: Grey out unrelated fields.
+        
+        panel.add(new JLabel("SVM type:"));
+        panel.add(svmType);
+        
+        panel.add(new JLabel("Kernel type:"));
+        panel.add(kernelType);
+        
+        panel.add(new JLabel("Enable shrinking?"));
+        panel.add(shrinking);
+        
+        panel.add(new JLabel("Train for probability estimates?"));
+        panel.add(probabilityEstimates);
+        
+        panel.add(new JLabel("Degree in kernel function:"));
+        degree = new JTextField(3);
+        panel.add(degree);
+        
+        panel.add(new JLabel("Gamma in kernel function:"));
+        gamma = new JTextField(8);
+        panel.add(gamma);
+        
+        panel.add(new JLabel("coef0 in kernel Function"));
+        coef0= new JTextField(8);
+        panel.add(coef0);
+        
+        panel.add(new JLabel("Cost (C) [0,3,4]:"));
+        cost = new JTextField(3);
+        panel.add(cost);
+        
+        panel.add(new JLabel("nu [1,2,4]:"));
+        nu = new JTextField(8);
+        panel.add(nu);
+        
+        panel.add(new JLabel("Epsilon in loss function [3]:"));
+        epsilonLoss = new JTextField(8);
+        panel.add(epsilonLoss);
+        
+        panel.add(new JLabel("Epsilon termination tolerance:"));
+        epsilonTolerance = new JTextField(8);
+        panel.add(epsilonTolerance);
+        
+        panel.add(new JLabel("Weight (see help) [0]:"));
+        weight = new JTextField(8);
+        panel.add(weight);
+        
+        panel.add(new JLabel("n-fold cross validation:"));
+        crossValidation = new JTextField(3);
+        panel.add(crossValidation);
+        
+        panel.add(new JLabel("Cache memory in MB:"));
+        cacheSize = new JTextField(6);
+        panel.add(cacheSize);
+		
+        // Set all options to their defaults
+        resetOptions();
+        
+        parent.add(panel);
+	}
+	
+	/**
+	 * Adds accompanying instructions, along with, help and reset buttons.
+	 * 
+	 * @param parent JPanel to contain the JPanel constructed.
+	 */
+	private void addRestrictionsHelp(JPanel parent) {
+		JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(1,2));
+        
+        panel.add(new JLabel("[0] -- C-SVC\n"
+        		+"[1] -- nu-SVC\n"
+        		+"[2] -- one-class SVM\n"
+        		+"[3] -- epsilon-SVR\n"
+        		+"[4] -- nu-SVR\n"));
+
+        addResetHelp(panel);
+        
+        parent.add(panel);
+	}
+	
+	/**
+	 * Adds help and reset buttons.
+	 * 
+	 * @param parent JPanel to contain the JPanel constructed.
+	 */
+	private void addResetHelp(JPanel parent) {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(2,1));
+		
+		// Label and add the help button
+		help = new JButton("Help");
+        panel.add(help);
+        
+        // Label and add the reset button
+        reset = new JButton("Reset");
+        panel.add(reset);
+        
+		parent.add(panel);
+	}
+	
+	/**
+	 * Adds mode buttons for scaling data, training model and predicting for 
+	 * given data. 
+	 * 
+	 * @param parent JPanel to contain the JPanel constructed.
+	 */
+	private void addModes(JPanel parent) {
+		JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(1,3));
+        
+        // Implement buttons and set text
+        scale = new JButton("Scale");
+        train = new JButton("Train");
+        predict = new JButton("Predict");
+        
+        panel.add(scale);
+        panel.add(train);
+        panel.add(predict);
+        
+        parent.add(panel);
+	}
+	
+	/**
+	 * Resets all the setting fields to their defaults.
+	 */
+	public void resetOptions() {
+		svmType.setSelectedIndex(0);
+		kernelType.setSelectedIndex(2);
+        shrinking.setSelectedIndex(1);
+        probabilityEstimates.setSelectedIndex(0);
+        degree.setText("3");
+        coef0.setText("0");
+        cost.setText("1");
+        nu.setText("0.5");
+        epsilonLoss.setText("0.1");
+        epsilonTolerance.setText("0.001");
+        weight.setText("1");
+        cacheSize.setText("100");
+        return;
+	}
+	
+	/**
+     * Allows for the help button to trigger actions. Sets up an ActionListener 
+     * with the passed in one.
+     * 
+     * @param p1 Given ActionListener for monitoring.
+     */
+    public void helpListener(ActionListener p1) {
+        help.addActionListener(p1);
+    }
+    
+    /**
+     * Allows for the reset button to trigger actions. Sets up an ActionListener 
+     * with the passed in one.
+     * 
+     * @param p1 Given ActionListener for monitoring.
+     */
+    public void resetListener(ActionListener p1) {
+        reset.addActionListener(p1);
+    }
 	
 	/**
      * Generic error dialogue box to be used for all exceptions, faults or 
@@ -100,5 +328,47 @@ public class LearningView extends JFrame {
         JOptionPane.showMessageDialog(this, errorDisplay, "Error", 
                 JOptionPane.ERROR_MESSAGE);
     }
-
+    
+    /**
+     * Displays a message box with the usage instructions for SVMlib
+     *  
+     */
+    public void helpUsage() {
+        //Creates a textarea with the help and usage information.
+        JTextArea helpText = new JTextArea(10,40);
+        helpText.setEditable(false);
+        helpText.setText("Options:\n"
+        		+"SVM type: set type of SVM (default 0)\n"
+        		+"	0 -- C-SVC		(multi-class classification)\n"
+        		+"	1 -- nu-SVC		(multi-class classification)\n"
+        		+"	2 -- one-class SVM\n"
+        		+"	3 -- epsilon-SVR	(regression)\n"
+        		+"	4 -- nu-SVR		(regression)\n"
+        		+"kernel_type : set type of kernel function (default 2)\n"
+        		+"	0 -- linear: u'*v\n"
+        		+"	1 -- polynomial: (gamma*u'*v + coef0)^degree\n"
+        		+"	2 -- radial basis function: exp(-gamma*|u-v|^2)\n"
+        		+"	3 -- sigmoid: tanh(gamma*u'*v + coef0)\n"
+        		+"	4 -- precomputed kernel (kernel values in training_set_file)\n"
+        		+"degree : set degree in kernel function (default 3)\n"
+        		+"gamma : set gamma in kernel function (default 1/num_features, leave box blank for default)\n"
+        		+"coef0 : set coef0 in kernel function (default 0)\n"
+        		+"cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR (default 1)\n"
+        		+"nu : set the parameter nu of nu-SVC, one-class SVM, and nu-SVR (default 0.5)\n"
+        		+"epsilon : set the epsilon in loss function of epsilon-SVR (default 0.1)\n"
+        		+"cachesize : set cache memory size in MB (default 100)\n"
+        		+"epsilon : set tolerance of termination criterion (default 0.001)\n"
+        		+"shrinking : whether to use the shrinking heuristics, 0 or 1 (default 1)\n"
+        		+"probability_estimates : whether to train a SVC or SVR model for probability estimates, 0 or 1 (default 0)\n"
+        		+"weight : set the parameter C of class i to weight*C, for C-SVC (default 1)\n"
+        		+": n-fold cross validation mode (leave blank for no cross validation)\n");
+        helpText.setCaretPosition(0);
+        //Adds the scrollpane implementing the textarea.
+        JScrollPane helpDisplay = new JScrollPane(helpText);
+        
+        /*Creates dialogue box with given message, title of "error" and an 
+         * error icon. */
+        JOptionPane.showMessageDialog(this, helpDisplay, "Help", 
+                JOptionPane.QUESTION_MESSAGE);
+    }
 }
